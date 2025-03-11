6b9d073aeecab4faeaa6c7efaee7ba5aa82fc130
George Appianing <george.adu.appianing@gmail.com>
2025-03-11 23:31:50 +0000

    Updated structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 function App() {
-  function Pizza() {
-    return (
-      <div>
-        <h2 className="flex text-center">I love pizza</h2>
-      </div>
-    );
-  }
   return (
     <>
       <div>
         <Pizza />
       </div>
     </>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2 className="flex text-center">I love pizza</h2>
+    </div>
   );
 }
 
